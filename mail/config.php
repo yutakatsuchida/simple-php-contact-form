31ef605cc4db785a2dc2569a5f13798683272359
yutakatsuchida <yutakatsuchida800@msn.com>
2018-06-17 22:40:14 -0700

    updated config.php and sendmail.php

--- mail/config.php
+++ mail/config.php
@@ -1,25 +1,63 @@
 <?php
-$encode="UTF-8";
-$email = "Your Email Address";
-// A notification email sent to you
-$to = $email;
-$from = $email;
-$subject = "You got an email";
+/*
+Configuration for sending an email
+*/
+
+// Language and Encode settings
+mb_language("English");
+mb_internal_encoding("UTF-8");
+
+// Messages
+$email_sccess_message = "Your message has been sent successfully!";
+$email_failed_message = "Failed to send your message. Please contact the webmaster.";
+
+// Your Email information that are used both in auto-send emails and notification emails.
+$your_email = "your@emailaddress.com";
+$your_name = "Your Name";
+$your_fullemail = $your_name . "<" . $your_email . ">";
 
 // A confirmation email sent to users automatically
 $auto_flag = true;
-$auto_from = $email;
+$auto_from_email = $your_email;
+$auto_from_name = $your_name;
+$auto_header = '';
+$auto_header .= "Content-Type: text/plain \r\n";
+$auto_header .= "Return-Path: " . $auto_from_email . " \r\n";
+$auto_header .= "From: " . $auto_from_email ." \r\n";
+$auto_header .= "Sender: " . $auto_from_email ." \r\n";
+$auto_header .= "Reply-To: " . $auto_from_email . " \r\n";
+$auto_header .= "Organization: " . $auto_from_name . " \r\n";
+$auto_header .= "X-Sender: " . $auto_from_email . " \r\n";
+$auto_header .= "X-Priority: 3 \r\n";
+$auto_from = $your_fullemail;
 $auto_subject = "Thank you for your inquiry";
 $auto_body = <<<__EOD__
+Dear {$_POST["fullname"]},
+
 Thank you for your inquiry.
+We review your message and respond to you as soon as possible.
 
-This is an email body.
-This is an email body.
-This is an email body.
-This is an email body.
+Support Team
+{$your_name}
+{$your_email}
+__EOD__;
 
-------------------------
-Your contact information
-------------------------
+
+// A notification email sent to you
+$to = $your_email;
+//$from = $your_fullemail;
+$subject = "You got an email";
+$post_massage = <<<__EOD__
+You received the following message from your customer.
+
+-----------------------------
+Full Name : {$_POST["fullname"]}
+Email : {$_POST["email"]}
+Tel : {$_POST["tel"]}
+Message : 
+{$_POST["message"]}
+-----------------------------
+
+This email is sent to you automatically by the system.
 __EOD__;
 ?>
--- mail/sendmail.php
+++ mail/sendmail.php
@@ -29,23 +29,55 @@
 		$errorFlag = true;
 	}
 
-	if ($errorFlag) {
+	
+
+	// There is no error, send an email
+	if (!$errorFlag) {
+		require_once("config.php");
+		$post_fullname = $_POST["fullname"];
+		$post_email = $_POST["email"];
+		$post_tel = $_POST["tel"];
+		$from = $post_fullname."<".$post_email.">";
+		$header = '';
+		$header .= "Content-Type: text/plain \r\n";
+		$header .= "Return-Path: " . $post_email . " \r\n";
+		$header .= "From: " . $post_email ." \r\n";
+		$header .= "Sender: " . $post_email ." \r\n";
+		$header .= "Reply-To: " . $post_email . " \r\n";
+		$header .= "Organization: " . $post_email . " \r\n";
+		$header .= "X-Sender: " . $post_email . " \r\n";
+		$header .= "X-Priority: 3 \r\n";
+		if (mb_send_mail($to, $subject, $post_massage, $header)) {
+			// if the message is sent
+			print $email_sccess_message;
+		} else {
+			// if the message is failed to send
+			print $email_failed_message;
+		}
+
+		//$auto_header = "From: {$auto_from}";
+		//echo $auto_mail, $auto_subject, $auto_body, $auto_header;
+		if($auto_flag){
+			$auto_mail = $post_email;
+			$auto_body = preg_replace("/\x0D\x0A|\x0D|\x0A/", "\n", $auto_body);
+			if (mb_send_mail($auto_mail, $auto_subject, $auto_body, $auto_header)){
+				// if the message is sent
+				print $email_sccess_message;
+			} else {
+				// if the message is failed to send
+				print $email_failed_message;
+			}
+			
+		}
+	} else {
+		// display an error message
 		$msg .= "<p>Please confirm the following information.</p>";
 		$msg .= "<ul>";
 		foreach ($errorMessages as $errorMessage) {
 			$msg .= "<li>" . $errorMessage . "</li>";
 		}
 		$msg .= "</ul>";
-		echo $msg;
-	} else {
-		require_once("config.php");
-		$auto_mail = $_POST["email"];
-		$header = "From: {$auto_from}";
-		if($auto_flag){
-			$auto_body = preg_replace("/\x0D\x0A|\x0D|\x0A/", "\n", $auto_body);
-			mb_send_mail($auto_mail, $auto_subject, $auto_body, $header);
-			echo "An email is sent";
-		}
+		print $msg;
 	}
 
 ?>
